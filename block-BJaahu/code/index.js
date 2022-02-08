9af41dc05b8bbe15f0260f4dc8120f6d2fcc1160
vandana <vandana123annusah@gmail.com>
2022-02-08 17:02:45 +0530

    completed block BJaahu

--- block-BJaahu/code/index.js
+++ block-BJaahu/code/index.js
@@ -4,36 +4,77 @@ let title = 'Where is the capital of Jordan';
 let options = ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'];
 let correctAnswerIndex = 1;
 
+
 // Prototypal pattern
 
-function isAnswerCorrect(index) {
-  return index === correctAnswerIndex;
-}
-
-function getCorrectAnswer() {
-  return options[correctAnswerIndex];
-}
-
-let question = {
-  title: 'Where is the capital of Jordan',
-  options: ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'],
-  correctAnswerIndex: 1,
-
-  isAnswerCorrect(index) {
+let questionMethods = {
+  isAnswerCorrect: function (index) {
     return index === this.correctAnswerIndex;
   },
 
-  getCorrectAnswer() {
+  getCorrectAnswer: function () {
     return this.options[this.correctAnswerIndex];
   },
 }
 
+function createQuestion (tittle,options,correctAnswerIndex) {
+  let question = Object.create(questionMethods);
+  question.tittle = tittle;
+  question.options = options;
+  question.correctAnswerIndex = correctAnswerIndex;
+
+  return question;
+}
+
+// Pseudoclassical Pattern
+
+function CreateQuestion(tittle,options,correctAnswerIndex) {
+  this.tittle = tittle;
+  this.options = options;
+  this.correctAnswerIndex = correctAnswerIndex;
+}
+
+CreateQuestion.prototype = {
+  isAnswerCorrect: function (index) {
+    return index === this.correctAnswerIndex;
+  },
+
+  getCorrectAnswer: function () {
+    return this.options[this.correctAnswerIndex];
+  },
+};
 
 
-let firstQuestion = new Question(
+// Class
+
+class CreateQuestion {
+  constructor(tittle,options,correctAnswerIndex) {
+  this.tittle = tittle;
+  this.options = options;
+  this.correctAnswerIndex = correctAnswerIndex;
+}
+
+
+
+  isAnswerCorrect(index) {
+    return index === this.correctAnswerIndex;
+  }
+
+  getCorrectAnswer() {
+    return this.options[this.correctAnswerIndex];
+  }
+};
+
+
+const firstQuestion = new CreateQuestion (
   'Where is the capital of Jordan',
   ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'],
   1
-
-  
 );
+
+let secondQuestion = new CreateQuestion(
+  'Where is the capital of Jamaica',
+  ['Tashkent', 'Amaan', 'Kingston', 'Nairobi'],
+  2
+);
+
